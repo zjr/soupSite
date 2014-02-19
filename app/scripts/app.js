495f67b0374e2d6706c00b4b4e8f34c747370e5e
ZJR <me@zjr.io>
2014-02-18 22:45:27 -0500

    Move nav & site title into layout, add about “page”.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,7 +9,10 @@ angular.module('SoupApp', [
 	.config(function ($routeProvider) {
 		$routeProvider
 			.when('/', {
-				templateUrl: 'views/main.html',
+				controller: 'MainCtrl'
+			})
+			.when('/about', {
+				templateUrl: 'views/about.html',
 				controller: 'MainCtrl'
 			})
 			.otherwise({
