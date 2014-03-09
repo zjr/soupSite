d5d4ef79aded764322057e4406a2ab01249385af
ZJR <me@zjr.io>
2014-03-08 20:53:12 -0500

    Remove unused controller.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,12 +9,8 @@ angular.module('SoupApp', [
 ])
 	.config(function ($routeProvider) {
 		$routeProvider
-			.when('/', {
-				controller: 'MainCtrl'
-			})
 			.when('/about', {
 				templateUrl: 'views/about.html',
-				controller: 'MainCtrl'
 			})
 			.otherwise({
 				redirectTo: '/'
--- app/scripts/controllers/main.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-angular.module('SoupApp')
-	.controller('MainCtrl', function ($scope) {
-	});
