c2bfa8dcdd0afe3ba5c36de81830dba3483d7404
ZJR <me@zjr.io>
2014-03-08 20:45:54 -0500

    Remove welcome console.log.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,6 +2,4 @@
 
 angular.module('SoupApp')
 	.controller('MainCtrl', function ($scope) {
-		$scope.welcomeMessage = 'Well how d’ye do?  Welcome to ZJR.IO!';
-		console.log($scope.welcomeMessage);
 	});
