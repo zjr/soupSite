49625032ca6ed56cb3d2790f9d31cf0ee7c8f8dd
ZJR <me@zjr.io>
2014-03-20 23:13:03 -0400

    Use videoBg directive to pick source format.

--- app/scripts/directives/videobg.js
+++ app/scripts/directives/videobg.js
@@ -1,12 +1,37 @@
 'use strict';
 
-angular.module('burgyBurgApp')
-  .directive('videoBg', function () {
-    return {
-      template: '<div></div>',
-      restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-        element.text('this is the videoBg directive');
-      }
-    };
-  });
+angular.module('SoupApp')
+    .directive('videoBg', function () {
+        return {
+            restrict: 'A',
+            replace: true,
+            scope: {
+                vidSrc: "=videoBg"
+            },
+            template: '<video class="vidBg" autoplay loop></video>',
+            link: function postLink(scope, elem, attrs) {
+
+                function getFormat() {
+                    var supportsWebm = elem[0].canPlayType('video/webm; codecs="vp8, vorbis"').replace(/^no$/,'');
+
+                    if(!supportsWebm) {
+                        return ['mp4'];
+                    } else if (supportsWebm === 'probably') {
+                        return ['webm'];
+                    } else {
+                        return ['webm', 'mp4'];
+                    }
+                }
+
+                (function loadSource() {
+                    var formats = getFormat();
+
+                    for(var i in formats) {
+                        var format = formats[i];
+                        var sourceElem = '<source src="' + scope.vidSrc + '.' + format + '" type="video/' + format + '">';
+                        elem.append(sourceElem);
+                    }
+                })();
+            }
+        };
+    });
