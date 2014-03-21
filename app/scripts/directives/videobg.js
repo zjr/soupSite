09dd0a1cbed362c28e9e74a0c17445f841737c7c
ZJR <me@zjr.io>
2014-03-21 00:26:13 -0400

    Use videoBg directive to skip loading of video on screens smaller than 1024px wide.

--- app/scripts/directives/videobg.js
+++ app/scripts/directives/videobg.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('SoupApp')
-    .directive('videoBg', function () {
+    .directive('videoBg', ['$window', function ($window) {
         return {
             restrict: 'A',
             replace: true,
@@ -9,7 +9,7 @@ angular.module('SoupApp')
                 vidSrc: "=videoBg"
             },
             template: '<video class="vidBg" autoplay loop></video>',
-            link: function postLink(scope, elem, attrs) {
+            link: function postLink(scope, elem) {
 
                 function getFormat() {
                     var supportsWebm = elem[0].canPlayType('video/webm; codecs="vp8, vorbis"').replace(/^no$/,'');
@@ -23,7 +23,19 @@ angular.module('SoupApp')
                     }
                 }
 
-                (function loadSource() {
+                function isSmallScreen(breakPoint) {
+                    var breakPointDefault = 1024;
+                    var breakPoint = typeof(breakPoint) === 'undefined' ? breakPointDefault : breakPoint;
+                    var winWidth = $window.innerWidth;
+
+                    if (winWidth < breakPoint) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+
+                function loadSource() {
                     var formats = getFormat();
 
                     for(var i in formats) {
@@ -31,7 +43,13 @@ angular.module('SoupApp')
                         var sourceElem = '<source src="' + scope.vidSrc + '.' + format + '" type="video/' + format + '">';
                         elem.append(sourceElem);
                     }
+                };
+
+                (function() {
+                    if(!isSmallScreen()) {
+                        loadSource();
+                    }
                 })();
             }
         };
-    });
+    }]);
