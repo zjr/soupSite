aec6f036147d837b5cd6aca491acc9ea2d8f4509
ZJR <me@zjr.io>
2014-04-06 14:06:21 -0400

    Move the work card test data into the Data factory.

--- app/scripts/controllers/work.js
+++ app/scripts/controllers/work.js
@@ -1,19 +1,6 @@
 'use strict';
 
 angular.module('SoupApp')
-    .controller('WorkCtrl', function ($scope) {
-        $scope.imgCol = [
-            {id: 'p1', 'title': 'Development', src: 'http://lorempixel.com/200/200/'},
-            {id: 'p2', 'title': 'Design & Development', src: 'http://lorempixel.com/300/500/', desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, repudiandae.'},
-            {id: 'p3', 'title': 'Photography', src: 'http://lorempixel.com/800/600/nightlife'},
-            {id: 'pX', 'title': 'Design', src: 'http://lorempixel.com/220/180'},
-            {id: 'pX', 'title': 'Some Title', src: 'http://lorempixel.com/400/400'},
-            {id: 'pX', 'title': 'Some Title', src: 'http://lorempixel.com/284/445'},
-            {id: 'pX', 'title': 'Some Title', src: 'http://lorempixel.com/235/322'},
-            {id: 'pX', 'title': 'Some Title', src: 'http://lorempixel.com/683/534'},
-            {id: 'pX', 'title': 'Some Title', src: 'http://lorempixel.com/1000/720'},
-            {id: 'pX', 'title': 'Some Title', src: 'http://lorempixel.com/289/300'},
-            {id: 'pX', 'title': 'Some Title', src: 'http://lorempixel.com/750/600'},
-            {id: 'pX', 'title': 'Some Title', src: 'http://lorempixel.com/683/734'}
-        ];
+    .controller('WorkCtrl', function ($scope, Data) {
+        $scope.workItems = Data.workItems;
     });
--- app/scripts/services/data.js
+++ app/scripts/services/data.js
@@ -3,6 +3,7 @@
 angular.module('SoupApp')
     .factory('Data', function () {
         var Data = {};
+
         Data.navItems = [
             {
                 name: 'About',
@@ -35,5 +36,21 @@ angular.module('SoupApp')
                 isExternal: true
             }
         ];
+
+        Data.workItems = [
+            {id: 'p1', 'title': 'Development', src: 'http://lorempixel.com/200/200/'},
+            {id: 'p2', 'title': 'Design & Development', src: 'http://lorempixel.com/300/500/', desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, repudiandae.'},
+            {id: 'p3', 'title': 'Photography', src: 'http://lorempixel.com/800/600/nightlife'},
+            {id: 'pX', 'title': 'Design', src: 'http://lorempixel.com/220/180'},
+            {id: 'pX', 'title': 'Some Title', src: 'http://lorempixel.com/400/400'},
+            {id: 'pX', 'title': 'Some Title', src: 'http://lorempixel.com/284/445'},
+            {id: 'pX', 'title': 'Some Title', src: 'http://lorempixel.com/235/322'},
+            {id: 'pX', 'title': 'Some Title', src: 'http://lorempixel.com/683/534'},
+            {id: 'pX', 'title': 'Some Title', src: 'http://lorempixel.com/1000/720'},
+            {id: 'pX', 'title': 'Some Title', src: 'http://lorempixel.com/289/300'},
+            {id: 'pX', 'title': 'Some Title', src: 'http://lorempixel.com/750/600'},
+            {id: 'pX', 'title': 'Some Title', src: 'http://lorempixel.com/683/734'}
+        ];
+
         return Data;
     });
