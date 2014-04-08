8f0326f45cf35e11746ceef756a769a20ed91daf
ZJR <me@zjr.io>
2014-04-08 13:15:18 -0400

    spCenter directive will recalculate positioning on window resize.

--- app/scripts/directives/sp-center.js
+++ app/scripts/directives/sp-center.js
@@ -1,15 +1,33 @@
 'use strict';
 
 angular.module('SoupApp')
-    .directive('spCenter', function () {
+    .directive('spCenter', function ($window) {
         return {
-            link: function postLink(scope, el) {
-                var width = el.prop('offsetWidth');
-                var height = el.prop('offsetHeight');
+            link: function postLink(scope, elem) {
 
-                el.css({
-                    'margin-left': -(width / 2) + 'px',
-                    'margin-top': -(height / 2) + 'px',
+                function getElemDimensions() {
+                    scope.offset = {};
+                    scope.offset.x = elem.prop('offsetWidth');
+                    scope.offset.y = elem.prop('offsetHeight');
+                }
+
+                function setElemMargins() {
+                    elem.css({
+                        'margin-left': -(scope.offset.x / 2) + 'px',
+                        'margin-top': -(scope.offset.y / 2) + 'px',
+                    });
+                }
+
+                scope.$watch(function(){ return scope.offset; }, function(newVal) {
+                    if (!newVal) {
+                        getElemDimensions();
+                    }
+                    setElemMargins();
+                });
+
+                angular.element($window).on('resize', function() {
+                    getElemDimensions();
+                    scope.$apply();
                 });
             }
         };
