e788879ed6570853b15f08e6bafd3b6f1aca2702
ZJR <me@zjr.io>
2014-04-08 22:05:02 -0400

    Ridiculous wrestling with the modal animations.

--- app/scripts/controllers/work.js
+++ app/scripts/controllers/work.js
@@ -1,6 +1,15 @@
 'use strict';
 
 angular.module('SoupApp')
-    .controller('WorkCtrl', function ($scope, Data) {
+    .controller('WorkCtrl', function ($scope, $state, $animate, Data) {
         $scope.data = Data;
+
+        $scope.closeModal = function () {
+            var elem = angular.element(document.querySelector('.modal-container'));
+
+            // TODO: this feels so dirty
+            $animate.addClass(elem, 'ng-hide', function () {
+                $state.go('^');
+            });
+        };
     });
